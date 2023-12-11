c67a9679738a4a744a3954530b9dc5bcce9807b1
Anton Platonov <platosha@gmail.com>
2023-12-11 17:38:28 +0200

    fix!: support Lit 3.0 (#15)
    * fix!: support Lit 3.0
    
    * chore: fix lint errors
    
    * chore: fix TypeScript errors
    
    * Apply suggestions from code review
    
    Co-authored-by: Vlad Rindevich <vladrin@vaadin.com>
    
    * chore: remove extra include from tsconfig.json
    
    * Apply suggestions from code review
    
    Co-authored-by: Vlad Rindevich <vladrin@vaadin.com>
    
    ---------
    
    Co-authored-by: Vlad Rindevich <vladrin@vaadin.com>

--- frontend/packages/common-frontend/test/ConnectionIndicator.test.ts
+++ frontend/packages/common-frontend/test/ConnectionIndicator.test.ts
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+import type { describe, it } from 'mocha';
 import { assert } from '@open-wc/testing';
 import { ConnectionIndicator, ConnectionState, ConnectionStateStore } from '../src';
 
